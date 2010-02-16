02e67f575e44f43656da7ba54c79f038a19b5ebe
djtal <guillaume.garcera@gmail.com>
2010-02-16 23:01:00 +0100

    initial work on export of data

--- exporter.rb
+++ exporter.rb
@@ -1,5 +1,16 @@
 require "erubis"
 
+
 class Exporter
+  
+  def export_member_per_game
+    ctx = {}
+    
+    
+    tpl = Erubis::Eruby.new(File.read("template/member_per_game.erb"))
+    File.open("export/member_per_game", "w") do |file|
+      tpl.evaluate(ctx)
+    end
+  end
   
 end
